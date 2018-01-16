6759588f0d6e68a4fb52fcdc0e8c7216f8bd827a
Kevin Boyle <papershack@gmail.com>
2018-01-15 22:49:31 -0800

    add remaining elements for functional page

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,31 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>My First Angular 2 App</h1>
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+    </ul>
   </div>
   `
 })
 //class declaration
 export class AppComponent {
-
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  //'this' is necessary b/c In Angular, when a variable in a component's class declaration references another variable in the class, it must be prefaced with the this keyword.
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course'),
+    new Task('Begin brainstorming possible JavaScript group projects'),
+    new Task('Add README file to last few Angular repos on GitHub')
+  ];
 }
+//below is our Task model
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string) { }
+}
+//because we've exported Task class, Task is now a valid data type for variables.
